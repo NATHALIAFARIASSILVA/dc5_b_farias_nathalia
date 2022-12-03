31d099944c9f995ef9f2b810619b95f3e8a3ed80
NATHALIA FARIAS <nathaliafarias1095@gmail.com>
2022-12-03 19:39:34 +0100

    FIN EXERCICE 4

---
+++ ex04/ft_print_is _negative.js
@@ -0,0 +1,13 @@
+function np(entier) {
+
+    let result;
+    if (entier >= 0) {
+        return "P";
+    } else if (entier < 0) {
+        return "N";
+    } else {
+        return "entrez un nombre"
+    };
+    return console.log(numbers);
+
+}
